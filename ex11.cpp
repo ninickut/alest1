1cbb6b40db3b982a5ef18469ccb6730f7b3c6276
ninickut <nicoletrindade@hotmail.com>
2025-08-08 12:21:41 -0300

    Lista 1 E11 Primeiro commit

---
+++ ex11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Data
+{
+    int dia;
+    int mes;
+    int ano;
+};
+
+struct Pessoa
+{
+    string nome;
+    double altura;
+    Data nascimento;
+};
+
+int main()
+{
+    Pessoa pessoas[10];
+
+}
